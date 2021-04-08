166c2f474d7dab66e366335989b76d93dcc9f43b
Taylor Galloway <gallowaytaylor@outlook.com>
2021-04-07 20:32:43 -0500

    Add arrays of food

---
+++ main.js
@@ -0,0 +1,66 @@
+/*Grab DOM Elements*/
+var foodTypeButton = document.querySelector('#food-type-button');
+var cookPot = document.querySelector('section image');
+
+
+/*Add event listeners*/
+foodTypeButton.addEventListener('click' randomDish)
+
+/*Add event handlers*/
+
+
+/*Global Variables*/
+
+/*Other Functions*/
+
+
+/*Data*/
+var sides = [
+  "Miso Glazed Carrots",
+  "Coleslaw",
+  "Garden Salad",
+  "Crispy Potatoes",
+  "Sweet Potato Tots",
+  "Coconut Rice",
+  "Caeser Salad",
+  "Shrimp Summer Rolls",
+  "Garlic Butter Mushrooms",
+  "Hush Puppies"
+];
+
+var mainDishes = [
+  "Spaghetti and Meatballs",
+  "Pineapple Chicken",
+  "Shakshuka",
+  "Thai Yellow Curry",
+  "Bibimbap",
+  "Chicken Parmesean",
+  "Butternut Squash Soup",
+  "BBQ Chicken Burgers",
+  "Ramen",
+  "Empanadas",
+  "Chicken Fried Rice",
+  "Sheet Pan Fajitas",
+  "Margarita Pizza"
+];
+
+var desserts = [
+"Apple Pie",
+"Lemon Meringue Pie",
+"Black Forest Cake",
+"Banana Bread",
+"Peach Cobbler",
+"Cheesecake",
+"Funfetti Cake",
+"Baklava",
+"Flan",
+"Macarons",
+"Macaroons",
+"Chocolate Cupcakes",
+"Pavlova",
+"Pumpkin Pie",
+"Key Lime Pie",
+"Tart Tatin",
+"Croissants",
+"Eclairs",
+];
